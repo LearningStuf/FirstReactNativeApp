77bb216ada038d3bd1a9a996b07ff56d4c7b5f85
Hassan Haroon <hassanwork12@gmail.com>
2023-03-31 15:01:22 +0100

    Adding Horizontal List to the code

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from "react";
 import { 
   View, 
   Text,
@@ -9,11 +10,16 @@ import {
 } from 'react-native'
 import { useRouter } from 'expo-router'
 
+
+
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
     <View>
       <View style = {styles.container}>
@@ -36,7 +42,27 @@ const Welcome = () => {
           style={styles.searchBtnImage}
           />
         </TouchableOpacity>
+      </View>
 
+      
+      <View>
+        <FlatList
+        data = {jobTypes}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style = {styles.tab(activeJobType,item)}
+            onPress={() => {
+              setActiveJobType(item)
+              router.push(`/search/${item}`);
+            }}
+          >
+            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle = {{columnGap: SIZES.small}}
+        horizontal
+        />
       </View>
     </View>
   )
