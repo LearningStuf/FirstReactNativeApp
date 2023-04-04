9c6c9cedddb8ad83c67acdbbbaa9ccbf556a7ad5
Hassan Haroon <hassanwork12@gmail.com>
2023-04-04 06:21:42 +0100

    Creating Horizontal recipe cards. This will be utilized to display saved and recently viewed recipes.

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -5,16 +5,21 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import  {COLORS, SIZES}  from '../../../constants';
 import PopulaJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
 
   const router = useRouter();
-  const isLoading  = false;
-  const error = false;
+  // const isLoading  = false;
+  // const error = false;
+
+  const {data, isLoading, error} = useFetch()
+
+  console.log(data)
   return (
     <View style = {styles.container}>
       <View style = {styles.header}>
-        <Text style = {styles.headerTitle}>Popular Jobs</Text>
+        <Text style = {styles.headerTitle}>Popular Recipies</Text>
         <TouchableOpacity>
           <Text style = {styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
